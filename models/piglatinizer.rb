b59d6f96cfdb372a201eba7984e8580e9f140ef2
remote-torrent-6747 <abbyc@sky.com>
2021-04-11 18:02:16 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,31 @@
+class PigLatinizer
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+    text_split
+  end
+
+  def text_split
+    @words = @text.split(" ")
+  end
+
+
+  def processor
+    processed_words = []
+    @words.each do |word|
+      if word.start_with?("a","e","i","o","u")
+        processed_words << "#{word}way"
+      else
+        until word.start_with?("a","e","i","o","u")
+          first_character = word[0]
+          word.slice!(0)
+          word = "#{word}#{first_character}"
+        end
+        processed_words << "#{word}ay"
+      end
+    end
+    processed_words.join(" ")
+  end
+
+end
